c252d465e93708a184bcefffa0393ce9d85037ed
sarif manung <406168026@yru.ac.th>
2021-11-14 07:43:47 +0700

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,34 @@ import styled from "styled-components";
 function Header() {
   return (
     <Nav>
-      <Logo src="" />
-      <NavMenu></NavMenu>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>Home</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span> WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span> ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIE</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://i.insider.com/617127ab33f4b300189ad412?width=1136&format=jpeg"></UserImg>
     </Nav>
   );
 }
@@ -15,9 +41,59 @@ export default Header;
 const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 
 const Logo = styled.img`
   width: 80px;
 `;
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform: scaleX(0);
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
