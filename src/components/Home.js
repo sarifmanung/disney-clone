51cb18142d01ecee298dd9ad6ae6195d92105fe3
sarif manung <406168026@yru.ac.th>
2021-11-15 20:30:03 +0700

    add viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <Containner>
+      <ImgSlider />
+      <Viewers/>
+    </Containner>
+  );
 }
 
-export default Home
+export default Home;
+
+const Containner = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow-x:hidden;
+
+  &:before {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    //z-index:-1 หมายถึงวางไว้ที่ พื้นหลัง
+    z-index: -1;
+  }
+`;
